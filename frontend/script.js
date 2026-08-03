f0004dfd42e197cdfd4bc143567a844c278ef465
Kristian Atanasov <124519664+atanasov23@users.noreply.github.com>
2026-08-03 23:14:25 +0300

    Добавяне на javasript код във файла

---
+++ frontend/script.js
@@ -0,0 +1,80 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+// Премахваме примерната задача от HTML
+taskList.innerHTML = [];
+
+// Добавяне на задача
+addTaskBtn.addEventListener("click", addTask);
+
+// Добавяне с Enter
+taskInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
+
+function addTask() {
+    const text = taskInput.value.trim();
+
+    if (text === "") {
+        alert("Моля, въведете задача.");
+        return;
+    }
+
+    createTask(text);
+
+    taskInput.value = "";
+    taskInput.focus();
+}
+
+function createTask(text) {
+
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    const buttons = document.createElement("div");
+
+    // Завършена задача
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = "✔";
+
+    completeBtn.addEventListener("click", () => {
+        span.style.textDecoration =
+            span.style.textDecoration === "line-through"
+                ? "none"
+                : "line-through";
+    });
+
+    // Редакция
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "✏";
+
+    editBtn.addEventListener("click", () => {
+        const newText = prompt("Редактирай задачата:", span.textContent);
+
+        if (newText && newText.trim() !== "") {
+            span.textContent = newText.trim();
+        }
+    });
+
+    // Изтриване
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑";
+
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+    buttons.appendChild(completeBtn);
+    buttons.appendChild(editBtn);
+    buttons.appendChild(deleteBtn);
+
+    li.appendChild(span);
+    li.appendChild(buttons);
+
+    taskList.appendChild(li);
+}
