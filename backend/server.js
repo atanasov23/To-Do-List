62ae1b9ecb87417073202293eb019ce8d4a04f1e
Kristian Atanasov <124519664+atanasov23@users.noreply.github.com>
2026-08-04 21:53:45 +0300

    Писане на код за node js сървър

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const FILE_PATH = path.join(__dirname, "data", "tasks.json");
+
+// Четене на задачите
+function loadTasks() {
+    if (!fs.existsSync(FILE_PATH)) {
+        return [];
+    }
+
+    const data = fs.readFileSync(FILE_PATH, "utf8");
+
+    return data ? JSON.parse(data) : [];
+}
+
+// Записване на задачите
+function saveTasks(tasks) {
+    fs.writeFileSync(FILE_PATH, JSON.stringify(tasks, null, 2));
+}
+
+// Връща всички задачи
+app.get("/tasks", (req, res) => {
+    const tasks = loadTasks();
+    res.json(tasks);
+});
+
+// Добавяне на задача
+app.post("/tasks", (req, res) => {
+
+    const tasks = loadTasks();
+
+    const newTask = {
+        id: Date.now(),
+        text: req.body.text,
+        completed: false
+    };
+
+    tasks.push(newTask);
+
+    saveTasks(tasks);
+
+    res.status(201).json(newTask);
+});
+// Стартиране на сървъра
+app.listen(PORT, () => {
+    console.log(`Сървърът работи на http://localhost:${PORT}`);
+});
